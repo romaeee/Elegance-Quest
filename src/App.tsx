093e7cdae592b2f2762ab92c096637b8a5659a6a
romaeee <romaeee@gmail.com>
2024-08-12 16:54:32 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ interface UserData {
 
 function App() {
   const [isLoading, setIsLoading] = useState(true); // Состояние загрузки
-  const [count, setCount] = useState<number>(0); // Счётчик
+  const [count, setCount] = useState<number | null>(null); // Счётчик
   const [userData, setUserData] = useState<UserData | null>(null);
 
   // Функция для загрузки данных из облачного хранилища
@@ -22,9 +22,12 @@ function App() {
       const result = await WebApp.cloudStorage.get('clickCount');
       if (result) {
         setCount(parseInt(result));
+      } else {
+        setCount(0); // Установить начальное значение, если данных нет
       }
     } catch (error) {
       console.error('Failed to load cloud data:', error);
+      setCount(0); // Установить начальное значение в случае ошибки
     }
   };
 
@@ -49,25 +52,27 @@ function App() {
     loadAppData();
   }, []);
 
-  // Сохранение данных в облачное хранилище
+  // Сохранение данных в облачное хранилище только после их загрузки
   useEffect(() => {
-    const saveCloudData = async () => {
-      try {
-        await WebApp.cloudStorage.set('clickCount', count.toString());
-      } catch (error) {
-        console.error('Failed to save cloud data:', error);
-      }
-    };
+    if (count !== null) {
+      const saveCloudData = async () => {
+        try {
+          await WebApp.cloudStorage.set('clickCount', count.toString());
+        } catch (error) {
+          console.error('Failed to save cloud data:', error);
+        }
+      };
 
-    saveCloudData();
+      saveCloudData();
+    }
   }, [count]);
 
   // Увеличение счётчика и сохранение
   const handleButtonClick = () => {
-    setCount((prevCount) => prevCount + 1);
+    setCount((prevCount) => (prevCount !== null ? prevCount + 1 : 1));
   };
 
-  if (isLoading) {
+  if (isLoading || count === null) {
     return <h1>Loading...</h1>;
   }
 
